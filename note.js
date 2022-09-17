c094382735fcd398d7518b7d0bf616271563f159
Eva Reitzel <evareitzel@gmail.com>
2022-09-17 13:25:16 -0400

    Done.

---
+++ note.js
@@ -0,0 +1,9 @@
+// const bio=props.bio
+  // let pTag
+  // if(bio){
+  //   pTag = <p>{props.bio}</p>
+  // }else{
+  //   pTag=""
+  // }
+
+  // {pTag}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function NavBar() {
+
   return (
     <nav>
       <a href="#home">Home</a>
